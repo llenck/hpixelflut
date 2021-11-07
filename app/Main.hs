f362e590989660d912fe59c966fc4fb47644fecd
Lia Lenckowski <lialenck@protonmail.com>
2021-11-07 15:21:09 +0100

    abstract recoloring

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 module Main where
 
 import System.IO
+import System.Environment
 import Control.Monad
 import Data.Maybe
 
 import Network.Simple.TCP
 import Codec.Picture (readImageWithMetadata, convertRGBA8, pixelAt, PixelRGBA8(PixelRGBA8))
-import Codec.Picture.Metadata as M (Keys(Width, Height), lookup)
+import Codec.Picture.Metadata as M (Metadatas, Keys(Width, Height), lookup)
 
 import FlutFmt
 import FlutIO (pixelflut, write_image, read_pixel_block)
@@ -16,29 +17,37 @@ fromRight :: Either a b -> b
 fromRight (Left _) = error "fromRight called on Left"
 fromRight (Right a) = a
 
+get_dims :: M.Metadatas -> Maybe (Int, Int)
 get_dims meta = do
     w <- M.lookup M.Width meta
     h <- M.lookup M.Height meta
-    pure (w, h)
+    pure (fromIntegral w, fromIntegral h)
 
-read_pixels im w h = map (\(x, y) -> (x, y, pixelAt im x y)) [(x, y) | x <- [0..w], y <- [0..h]]
+read_pixels im w h = map (\(x, y) -> (x, y, pixelAt im x y)) [(x, y) | x <- [0..w - 1], y <- [0..h - 1]]
 
 dump_rgba8 (PixelRGBA8 r g b a) = (r, g, b, a)
 
+read_im :: FilePath -> IO [(Int, Int, Int, Int, Int)]
 read_im path = do
     im_ <- readImageWithMetadata path
     let (im, meta) = fromRight im_
     let im8 = convertRGBA8 im
     let (w, h) = fromJust $ get_dims meta
-    let pixels = read_pixels im8 (fromIntegral w) (fromIntegral h)
-    pure $ map (\(x, y, c) -> (\(r, g, b, a) -> (x, y, r, g, b, a)) $ dump_rgba8 c) pixels
+    let pixels = read_pixels im8 w h
+    let fmt_pixels = map (\(x, y, c) -> (\(r, g, b, a) -> (x, y, r, g, b, a)) $ dump_rgba8 c) pixels
+    pure $ map (\(x, y, r, g, b, a) -> (x, y, fi r, fi g, fi b)) $ filter (\(x, y, r, g, b, a) -> a > 127) fmt_pixels
+    where fi = fromIntegral
+
+move_im dx dy = map (\(x, y, r, g, b) -> (x + dx, y + dy, r, g, b))
+recolor_im (r, g, b) = map (\(x, y, _, _, _) -> (x, y, r, g, b))
+black_im = recolor_im (0, 0, 0)
 
 plot_fn f = [(x, floor y, r, g, b) |
     x <- [0..1280],
     let n_x = fromIntegral x / 1280,
     let y = 360 + f n_x * (-360),
     --let (r, g, b) = (255, 255, 255)]
-    let (r, g, b) = rainbow x $ floor y]
+    let (r, g, b) = rainbow $ floor y]
 
 sine_plot _ = pure $ plot_fn (sin . (*6.283))
 
@@ -59,42 +68,42 @@ clear _ = pure $ [(x, y, 0, 0, 0) | x <- [0..1280], y <- [0..720]]
 
 block x y w h r g b = [(x, y, r, g, b) | x <- [x..x + w], y <- [y..y + h]]
 
-paddle_im x y (r, g, b) =
-    (block x y 10 100 r g b) ++
-    (block x (y - 10) 10 10 0 0 0) ++
-    (block x (y + 100) 10 10 0 0 0) ++
-    (block (x-10) y 10 100 0 0 0) ++
-    (block (x+10) y 10 100 0 0 0)
+paddle_im = block 0 0 10 100 255 255 255
 
-wand_im x y (r, g, b) =
-    (block 30 y 1200 5 r g b) ++
-    (block 30 (y+10) 1200 10 0 0 0) ++
-    (block 30 (y-10) 1200 10 0 0 0)
-
-paddle :: Int -> (Socket, SockAddr) -> IO ()
-paddle off (s, _) = do
+paddle :: [(Int, Int, Int, Int, Int)] -> (Socket, SockAddr) -> IO ()
+paddle im (s, _) = do
     hSetBuffering stdin NoBuffering
-    paddle_ s off 310 0
+    paddle_ s im 0
     pure ()
-    where paddle_ s x y counter = do
+    where paddle_ s im c = do
             ch <- getChar
-            let new_y = flip mod 720 $ y + case ch of
+            let dy = case ch of
                     'w' -> -10
                     's' -> 10
+                    'W' -> -50
+                    'S' -> 50
                     _   -> 0
 
-            let new_x = flip mod 1280 $ x + case ch of
+            let dx = case ch of
                     'a' -> -10
                     'd' -> 10
+                    'A' -> -50
+                    'D' -> 50
                     _   -> 0
 
-            let im = paddle_im new_x new_y $ rainbow 0 counter
-            --let im2 = wand_im new_x new_y $ rainbow 0 counter
-            write_image s im
+            let n_im = {- recolor_im (rainbow c) $ -} move_im dx dy im
+            write_image s $ black_im im
+            write_image s n_im
 
-            paddle_ s new_x new_y (counter + 1)
+            paddle_ s n_im (c + 1)
 
 main = do
     --pixelflut "pixelflut.uwu.industries" "1234" $ [\_ -> pure $ block 0 710 1280 10 255 255 255]
-    connect "pixelflut.uwu.industries" "1234" $ paddle 1000
+    --pixelflut "pixelflut.uwu.industries" "1234" $ [clear]
+    args <- getArgs
+    im <- case (listToMaybe args) of
+        Nothing -> pure paddle_im
+        Just s -> read_im s
+
+    connect "pixelflut.uwu.industries" "1234" $ paddle im
     putStrLn "Done :)"
--- src/Rainbow.hs
+++ src/Rainbow.hs
@@ -1,10 +1,9 @@
 module Rainbow where
 
 freq = 0.1
-rainbow :: Int -> Int -> (Int, Int, Int)
-rainbow x y = (r n, g n, b n)
-    where n = x + y
-          sin_arg off = freq * (fromIntegral n) + pi / 1.5 * off
+rainbow :: Int -> (Int, Int, Int)
+rainbow x = (r x, g x, b x)
+    where sin_arg off = freq * (fromIntegral x) + pi / 1.5 * off
           single_color off n = floor $ sin (sin_arg off) * 127 + 128
           r = single_color 0
           g = single_color 1
